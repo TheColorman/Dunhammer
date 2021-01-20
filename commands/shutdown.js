4ec425309d64bd260ac1666231c95ef839f2bce3
TheColorman <thoren.alex@gmail.com>
2021-01-20 11:09:22 +0100

    :sparkles: Added ℌ𝔢𝔞𝔡9𝔵 to shutdown IDs

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -5,7 +5,7 @@ module.exports = {
     long_desc: 'Shuts down the bot. The bot cannot be turned on through Discord, and this command can only be used by the bot owner',
     execute(msg, args, tags, databases) {
         const guild = databases.guilds.findOne({ guild_id: msg.guild.id });
-        if (['298842558610800650'].includes(msg.author.id.toString())) {
+        if (['298842558610800650', '411240035841474590'].includes(msg.author.id.toString())) {
             msg.channel.send({ embed: {
                 "color": 2215713,
                 "title": ":zzz: Shutting down..."
