25762b454f5ce24bb980fd374756da8f6448ca27
TheColorman <thoren.alex@gmail.com>
2021-01-21 22:29:19 +0100

    :zap: Improved performance by not getting rate limited

--- commands/reloaddatabase.js
+++ commands/reloaddatabase.js
@@ -14,10 +14,8 @@ module.exports = {
         }
         const message = await msg.channel.send({ embed: {
             color: 49919,
-            description: ":arrows_counterclockwise: Refreshing database... (This ***will*** take a while)."
+            description: ":arrows_counterclockwise: Refreshing database... (This might take a while)."
         }});
-        const description = [];
-        const max_desc_length = 15;
 
         // Guild
         const db_guild = databases.guilds.findOne({ guild_id: msg.guild.id });
@@ -29,26 +27,13 @@ module.exports = {
         // Users (from guild)
         const members = await msg.guild.members.cache;
         const members_length = members.size;
-        description.push(`The guild contains ${members_length} members.`);
-
-        let members_index = 0;
-        if (description.length > max_desc_length) description.shift();
+        await message.edit({ embed: {
+            color: 49919,
+            description: `:arrows_counterclockwise: Refreshing database... (This might take a while).\n\n\`\`\`\nRefreshing roles for ${members_length} members...\n\`\`\``
+        }});
         members.each(async member => {
-            members_index++;
-            description.push(`Updating roles for ${member.user.tag}.`);
-            if (description.length > max_desc_length) description.shift();
-            await message.edit({ embed: {
-                color: 49919,
-                description: `:arrows_counterclockwise: Refreshing database... (This ***will*** take a while).\n\n\`\`\`\n      Member ${members_index}/${members_length}\n${description.join('\n')}\n\`\`\``
-            }});
             // roles
             if (databases.users.findOne({ user_id: member.id}) == null) {
-                description.push(`${member.user.tag} not in database, creating entry...`);
-                if (description.length > max_desc_length) description.shift();
-                await message.edit({ embed: {
-                    color: 49919,
-                    description: `:arrows_counterclockwise: Refreshing database... (This ***will*** take a while).\n\n\`\`\`\n      Member ${members_index}/${members_length}\n${description.join('\n')}\n\`\`\``
-                }});    
                 databases.users.insert({
                     user_id: member.id,
                     xp: 0,
@@ -68,48 +53,23 @@ module.exports = {
         });
         // Users (from database)
         const db_users = databases.users.chain().data();
-        const db_users_length = databases.users.chain().data().length;
-        description.push(`Verifying database entries...`);
-        if (description.length > max_desc_length) description.shift();
-        description.push(`The database contains ${db_users_length} users.`);
-        if (description.length > max_desc_length) description.shift();
-
+        const db_users_length = db_users.length;
         await message.edit({ embed: {
             color: 49919,
-            description: `:arrows_counterclockwise: Refreshing database... (This ***will*** take a while).\n\n\`\`\`\n${description.join("\n")}\n\`\`\``
-        }});
-        let db_users_index = 0;
+            description: `:arrows_counterclockwise: Refreshing database... (This might take a while).\n\n\`\`\`\nRefreshing roles for ${members_length} members...\nVerifying ${db_users_length} users in database...\n\`\`\``
+        }});        
         for (const db_user of db_users) {
-            db_users_index++;
-            description.push(`Searching for guild member with ID ${db_user.user_id}...`);
-            if (description.length > max_desc_length) description.shift();    
-            await message.edit({ embed: {
-                color: 49919,
-                description: `:arrows_counterclockwise: Refreshing database... (This ***will*** take a while).\n\n\`\`\`\n      User ${db_users_index}/${db_users_length}\n${description.join("\n")}\n\`\`\``
-            }});    
             try {
                 const db_member = await msg.guild.members.fetch(db_user.user_id);
-                description.push(`Matching user found: ${db_member.user.tag}`);
-                if (description.length > max_desc_length) description.shift(); 
-                await message.edit({ embed: {
-                    color: 49919,
-                    description: `:arrows_counterclockwise: Refreshing database... (This ***will*** take a while).\n\n\`\`\`\n      User ${db_users_index}/${db_users_length}\n${description.join("\n")}\n\`\`\``
-                }});        
             } catch (err) {
                 if (err.message === "Unknown Member" || err.message === "Unknown User") {
                     db_user.inGuild = false;
                 }
-                description.push(`No users found in guild, marking entry MISSING`);
-                if (description.length > max_desc_length) description.shift();
-                await message.edit({ embed: {
-                    color: 49919,
-                    description: `:arrows_counterclockwise: Refreshing database... (This ***will*** take a while).\n\n\`\`\`\n      User ${db_users_index}/${db_users_length}\n${description.join("\n")}\n\`\`\``
-                }});        
             }
         }
         await message.edit({ embed: {
-            color: 2215713,
-            description: `~~:arrows_counterclockwise: Refreshing database... (This ***will*** take a while).~~\n\n\`\`\`\n${description.join("\n")}\n\`\`\`\n\n:white_check_mark: Done!`
+            color: 49919,
+            description: `:arrows_counterclockwise: Refreshing database... (This might take a while).\n\n\`\`\`\nRefreshing roles for ${members_length} members...\nVerifying ${db_users_length} users in database...\n\`\`\`\n\n:white_check_mark: Done!`
         }});
         return msg.channel.send({ embed: {
             color: 2215713,
