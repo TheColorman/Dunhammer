535797c83c1fc8299f7f8c7efeaf252913b930a4
TheColorman <thoren.alex@gmail.com>
2021-02-21 00:45:15 +0100

    :pencil2: Fixed command description, usage and permissions

--- commands/1📄 General/roadmap.js
+++ commands/1📄 General/roadmap.js
@@ -3,9 +3,8 @@ module.exports = {
     name: 'roadmap',
     short_desc: 'Sends a link to the roadmap.',
     aliases: ['trello', 'todo', 'to-do'],
-    long_desc: 'If this is set to true, Dunhammer will treat other bots as regular users.',
-    usage: '[true/false]',
-    permissions: 'BAN_MEMBERS',
+    long_desc: 'Sends a to the Dunhammer roadmap on trello.com. Shows all coming planned features, bugs, what is currently being worked on and all the bots features, along with the bot version and beta features.',
+    usage: '',
     cooldown: 2,
     async execute(msg, args, tags, databases, interaction) {
         if (interaction) {  // Acknowledge slash command if it exists
