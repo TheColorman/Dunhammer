a81a2aa516404457a6baf5393831ae860d29d9c0
TheColorman <thoren.alex@gmail.com>
2021-02-24 01:03:50 +0100

    :bug: Defined guild and fixed wrong permission error description

--- commands/6🚷 Owner only/debug.js
+++ commands/6🚷 Owner only/debug.js
@@ -5,11 +5,12 @@ module.exports = {
     short_desc: 'Debug command.',
     long_desc: 'Debug command. Can only be used by bot owner, and is a placeholder for random testing.',
     execute(msg, args, tags, databases) {
+        const guild = databases.guilds.findOne({ guild_id: msg.guild.id });
         if (!['298842558610800650', '411240035841474590'].includes(msg.author.id)) {
             return msg.channel.send({ embed: {
                 "color": 0xcf2d2d,
                 "title": ":octagonal_sign: Error!",
-                "description": `:no_entry: You don't have access to \`${guild.prefix}shutdown\`!`
+                "description": `:no_entry: You don't have access to \`${guild.prefix}debug\`!`
             }});
         }
 
