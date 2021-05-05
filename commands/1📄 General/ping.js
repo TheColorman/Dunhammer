60e8aa8aa33b33d9196f4e404be6a52d17b8ed0e
TheColorman <thoren.alex@gmail.com>
2021-05-05 19:10:37 +0200

    :bug: Fixed ping calculation
    Actually, it got removed in a previous update so it was just added back with a different calculation method

--- commands/1📄 General/ping.js
+++ commands/1📄 General/ping.js
@@ -14,19 +14,18 @@ module.exports = {
             }});
         }
         const sound = msg.content.includes("ping") ? "Pong" : "Ping";
+        const pingCalc = await msg.channel.send("ping calculator");
+        pingCalc.delete();
+        const ping = (new Date(pingCalc.id / 4194304 + 1420070400000)) - (new Date(msg.id / 4194304 + 1420070400000));
         const replyEmbed = {
             color: 2215713,
             description: `:ping_pong: ${sound}! \`(${ping} ms)\``,
         }
+
         if (interaction) {
             return await apiFunctions.interactionEdit(msg.client, interaction, msg.channel, replyEmbed);
         } else {
             return msg.channel.send({ embed: replyEmbed});
         }
-        msg.channel.send({ embed: reply_embed}).then((reply) => {
-            const ping = (new Date(reply.id / 4194304 + 1420070400000)) - (new Date(msg.id / 4194304 + 1420070400000));
-            reply_embed.description = `:ping_pong: ${sound}! \`(${ping} ms)\``;
-            reply.edit({ embed: reply_embed});
-        });
     }
 }
