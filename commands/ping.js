15e1a821eddb5f3e112ecc618210d021879e19a4
Alexander Thoren <thoren.alex@gmail.com>
2021-08-18 10:20:33 +0200

    :children_crossing: Replaced button collector with permanent solution

--- commands/ping.js
+++ commands/ping.js
@@ -23,68 +23,45 @@ module.exports = {
             }], 
             fetchReply: true 
         }).then(async (reply) => {
-            const ping = await reply.edit({
-                    embeds: [{
-                        "color": 0x7BA043,
-                        "description": `Pong! \`(${reply.createdTimestamp - interaction.createdTimestamp} ms)\``
-                    }],
+            reply.edit({
+                embeds: [{
+                    "color": 0x7BA043,
+                    "description": `Pong! \`(${reply.createdTimestamp - interaction.createdTimestamp} ms)\``
+                }],
+                components: [{
+                    type: "ACTION_ROW",
                     components: [{
-                        type: "ACTION_ROW",
-                        components: [{
-                            type: "BUTTON",
-                            label: "Measure again",
-                            customId: "commands.ping",
-                            style: "SUCCESS"
-                        }]
-                    }],
-                    fetchReply: true
-                }),
-
-                collector = ping.createMessageComponentCollector({ componentType: "BUTTON", time: 30000 });
-
-            collector.on('collect', buttonInteraction => {
-                buttonInteraction.update({
-                    embeds: [{
-                        "color": 0x2F3136,
-                        "description": `<a:discord_loading:821347252085063680>`
-                    }], 
-                    fetchReply: true 
-                }).then(async (collectedReply) => {
-                    await collectedReply.edit({
-                        embeds: [{
-                            "color": 0x7BA043,
-                            "description": `Pong! \`(${collectedReply.editedTimestamp - buttonInteraction.createdTimestamp} ms)\``
-                        }],
-                        components: [{
-                            type: "ACTION_ROW",
-                            components: [{
-                                type: "BUTTON",
-                                label: "Measure again",
-                                customId: "commands.ping",
-                                style: "SUCCESS"
-                            }]
-                        }]
-                    });
-                });
-            });
-
-            collector.on('end', () => {
-                ping.edit({
-                    embeds: [{
-                        "color": 0x7BA043,
-                        "description": `Pong! \`(${reply.createdTimestamp - interaction.createdTimestamp} ms)\``
-                    }],
-                    components: [{
-                        type: "ACTION_ROW",
-                        components: [{
-                            type: "BUTTON",
-                            label: "Measure again",
-                            customId: "commands.ping",
-                            style: "SUCCESS",
-                            disabled: true
-                        }]
+                        type: "BUTTON",
+                        label: "Measure again",
+                        customId: "commands.ping.repeat",
+                        style: "SUCCESS"
                     }]
-                });
+                }]
+            });
+        });
+    },
+    async repeat(interaction) {
+        interaction.update({
+            embeds: [{
+                "color": 0x2F3136,
+                "description": `<a:discord_loading:821347252085063680>`
+            }], 
+            fetchReply: true 
+        }).then(async (reply) => {
+            reply.edit({
+                embeds: [{
+                    "color": 0x7BA043,
+                    "description": `Pong! \`(${reply.editedTimestamp - interaction.createdTimestamp} ms)\``
+                }],
+                components: [{
+                    type: "ACTION_ROW",
+                    components: [{
+                        type: "BUTTON",
+                        label: "Measure again",
+                        customId: "commands.ping.repeat",
+                        style: "SUCCESS"
+                    }]
+                }]
             });
         });
     }
