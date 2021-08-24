3322b1550ecc422c799a6169092cc22c6cfca34b
TheColorman <thoren.alex@gmail.com>
2021-08-24 16:35:00 +0200

    :fire: Removed DIRECT_MESSAGES intent

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const { Client, Intents, Collection, Message } = require('discord.js'),
     fs = require('fs'),
     MySQL = require('./sql/sql.js'),
 
-    client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS], partials: ['CHANNEL'] });
+    client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS] });
 
 // Load commands
 client.commands = new Collection();
