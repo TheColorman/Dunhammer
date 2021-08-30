af021795d447e4040354e2483630d74423b0c9b7
TheColorman <thoren.alex@gmail.com>
2021-08-31 00:21:14 +0200

    :construction: Updated leaderboard command

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -60,20 +60,53 @@ module.exports = {
                 }], 
                 fetchReply: true 
             }),
-            user = interaction.options.getUser('user') || interaction.user,
-            role = interaction.options.getRole('role'),
-            filter = interaction.options.getString('filter'),
-            type = interaction.options.getString('type') || "server",
+            type = interaction.options.getString('type') || "server";
 
+        if (type == "server") await replyServer(interaction, sql, reply, 1);
+        else await replyGlobal(interaction, sql, reply, 1);
+    },
+    /**
+     * @param {Discord.ButtonInteraction} interaction 
+     * @param {Number} currentPage 
+     */
+    async previous(interaction, sql, dataString) {
+        /** Oh boy I'm gonna need a multi-line comment for this one.
+         * So, basically, the function replyServer (and replyGlobal when I finish that one)
+         * excpects the first parameter to be an interaction. That's all good, but only CommandInteractions have the .options 
+         * property,  so when  interaction.options  is checked,  it's  always  going  to  return  undefined  when checking  a
+         * ButtonInteraction.  Now, what if someone uses a role  when doing the CommandInteraction  and they press a  button?
+         * The  ButtonInteraction won't have  a role,  and all  further pages aren't  going to filter by  the role.  That's a
+         * problem. So, I  have added a .options property  to the ButtonInteraction with the  methods used in the replyServer
+         * function,  and they return the value  from the data object, which is a  stringified object passed as a part of the
+         * buttons  CustomId.  This CustomId  is  defined  at the  bototm of  the replyServer  function,  called  dataString.
+         * Thank you for coming to my Ted Talk.
+         * 
+         * TL;DR: I'm creating fake methods and returning stringified objects.
+         */
 
-            { list, page } = await getServer(interaction, sql);
-
-        reply.edit({
-            embeds: [{
-                title: `🏆 ${interaction.guild.name}'s Leaderboard`,
-                description: `<@${list.map(member => member.userid).join(">\n<@")}>`
-            }]
-        });
+        const data = JSON.parse(dataString);
+        interaction.options = {
+            getUser: (_user) => undefined,
+            getRole: (_role) => data.role ? interaction.guild.roles.fetch(data.role) : undefined,
+            getString: (_filter) => data.filter
+        }
+        if (data.type == "server") await replyServer(interaction, sql, interaction, data.page - 1);
+        else await replyGlobal(interaction, sql, interaction, data.page - 1);
+    },
+    /**
+     * @param {Discord.ButtonInteraction} interaction 
+     * @param {Number} currentPage 
+     */
+    async next(interaction, sql, dataString) {
+        const data = JSON.parse(dataString);
+        interaction.options = {
+            getUser: (_user) => undefined,
+            getRole: (_role) => data.role ? interaction.guild.roles.fetch(data.role) : undefined,
+            getString: (_filter) => data.filter
+        }
+        //                                                                                    ⬇ fuck javascript, all my homies hate javascript
+        if (data.type == "server") await replyServer(interaction, sql, interaction, data.page - - 1);
+        else await replyGlobal(interaction, sql, interaction, data.page - - 1);
     }
 }
 
@@ -82,14 +115,17 @@ module.exports = {
  * @param {MySQL} sql 
  * @param {String} page 
  */
-async function getServer(interaction, sql, page) {
-    page ||= 1;
+async function replyServer(interaction, sql, reply, page) {
     const
-        user = interaction.options.getUser('user') || interaction.user,
+        // Get general info
+        user = interaction.options ? interaction.options.getUser('user') || interaction.user : interaction.user,
         member = await interaction.guild.members.fetch(user),
-        DBGuildMember = await sql.getDBGuildMember(member),
-        role = interaction.options.getRole('role'),
-        filter = interaction.options.getString('filter'),
+        // Variable isn't used, but the program needs to verify
+        // that the member is in the database or it wont be
+        // able to find the member later
+        _DBGuildMember = await sql.getDBGuildMember(member),
+        role = interaction.options ? await interaction.options.getRole('role') : undefined,
+        filter = interaction.options ? interaction.options.getString('filter') : undefined,
 
         GuildMemberDB = await sql.get("guildusers", `guildid = ${interaction.guild.id}`, `xp DESC`),
         // Filter by role, check if filter is blacklist and continue accordingly
@@ -98,13 +134,132 @@ async function getServer(interaction, sql, page) {
                 !JSON.parse(DBGuildMember.roles).includes(role.id) :
                 JSON.parse(DBGuildMember.roles).includes(role.id)) :
             GuildMemberDB,
-        memberIndex = GuildMemberDBFiltered.findIndex(DBGuildMember => DBGuildMember.userid == user.id),
+        // Maximum amount of pages based on how many entries are in the database
         maxPage = Math.ceil(GuildMemberDBFiltered.length / 10),
-        currentPage = Math.min(page, maxPage),
-        topTen = GuildMemberDBFiltered.splice(10 * (currentPage - 1), currentPage * 10);
-    
-    return {
-        list: topTen,
-        page: currentPage
-    }
+        currentPage = page > maxPage ? 1 : page < 1 ? maxPage : page,
+        // Calculate the 10 members shown based on the page we are on
+        DBGuildMemberList = GuildMemberDBFiltered.splice(10 * (currentPage - 1), 10),
+        // Map the array into both DiscordJS GuildMembers and DBGuildMembers for use in ...
+        MultiMemberList = DBGuildMemberList.map(async DBGuildMember => {
+            if (!DBGuildMember.inGuild) return [DBGuildMember.nickname, DBGuildMember];
+            return [await interaction.guild.members.fetch(DBGuildMember.userid), DBGuildMember];
+        }),
+        // ... this array, where it's all turned into a string with the format "#1 - {DiscordGuildMember} `1234` xp"
+        GuildMemberDisplay = (await Promise.all(MultiMemberList)).map((memberArr, index) => {
+            const
+                mentioned = memberArr[1].userid == user.id,
+                added = mentioned ? "__" : "";
+            // Calculation for rank is based on index and page, so page 2 has from rank 11 to 20
+            return `${added}#${(currentPage - 1) * 10 + index + 1} - ${memberArr[0]} \`${memberArr[1].xp}\` xp${added}`
+        }),
+
+
+        dataString = JSON.stringify({
+            page: currentPage,
+            role: role? role.id : undefined,
+            filter: filter,
+            type: "server"
+        }),
+        roleString = `${
+            // If filtered by role
+            role ?  // add "Members with" string
+                `**Members with${ // if the role is blacklisted, add "out", so the string becomes: "Members without"
+                    filter && filter == "blacklist" ?
+                        'out' :
+                        ''
+                // Add " role {role}" to the string, string is now: "Members with(out) role {role}"
+                } role ${role}**\n\n` :
+                // Or don't have a string if there is no filter
+                ''
+        }`,
+        replyObj = {
+            embeds: [{
+                title: `🏆 ${interaction.guild.name}'s Leaderboard`,
+                description: `${roleString}${GuildMemberDisplay.join("\n")}`,
+                footer: {
+                    text: `Page ${currentPage}/${maxPage}`,
+                    iconURL: interaction.guild.iconURL({ size: 16 })
+                }
+            }],
+            components: [{
+                type: "ACTION_ROW",
+                components: [{
+                    type: "BUTTON", 
+                    customId: `commands.leaderboard.previous.${dataString}`, // yeah stringifying an object is a stupid-ass solution but why are you reading the fucking source code?
+                    emoji: "◀",
+                    style: "PRIMARY"
+                }, {
+                    type: "BUTTON",
+                    customId: `commands.leaderboard.next.${dataString}`,
+                    emoji: "▶",
+                    style: "PRIMARY"
+                }]
+            }]
+        }
+
+    if (interaction.isButton()) return reply.update(replyObj);
+    reply.edit(replyObj);
+}
+/**
+ * @param {Discord.CommandInteraction|Discord.ButtonInteraction} interaction 
+ * @param {MySQL} sql 
+ * @param {Discord.Message|Discord.ButtonInteraction} reply 
+ * @param {Number} page 
+ */
+async function replyGlobal(interaction, sql, reply, page) {
+    const
+        // Get general info
+        user = interaction.options ? interaction.options.getUser('user') || interaction.user : interaction.user,
+        // Variable isn't used, but the program needs to verify
+        // that the user is in the database or it wont be
+        // able to find the user later
+        _DBUser = await sql.getDBUser(user),
+        UserDB = await sql.get("users", ``, `xp DESC`),
+        // Filter by role, check if filter is blacklist and continue accordingly
+        // Maximum amount of pages based on how many entries are in the database
+        maxPage = Math.ceil(UserDB.length / 10),
+        currentPage = page > maxPage ? 1 : page < 1 ? maxPage : page,
+        // Calculate the 10 members shown based on the page we are on
+        DBUserList = UserDB.splice(10 * (currentPage - 1), 10),
+        // ... this array, where it's all turned into a string with the format "#1 - {DiscordGuildMember} `1234` xp"
+        UserDisplay = DBUserList.map((DBUser, index) => {
+            const
+                mentioned = DBUser.id == user.id,
+                added = mentioned ? "__" : "";
+            // Calculation for rank is based on index and page, so page 2 has from rank 11 to 20
+            return `${added}#${(currentPage - 1) * 10 + index + 1} - ${DBUser.username} \`${DBUser.xp}\` xp${added}`
+        }),
+
+
+        dataString = JSON.stringify({
+            page: currentPage,
+            type: "global"
+        }),
+        replyObj = {
+            embeds: [{
+                title: `🏆 Dunhammer Global Leaderboard <:DunhammerCoin:878740195078463519>`,
+                description: `${UserDisplay.join("\n")}`,
+                footer: {
+                    text: `Page ${currentPage}/${maxPage}`,
+                    iconURL: interaction.client.user.displayAvatarURL({ size: 16 })
+                }
+            }],
+            components: [{
+                type: "ACTION_ROW",
+                components: [{
+                    type: "BUTTON", 
+                    customId: `commands.leaderboard.previous.${dataString}`, // yeah stringifying an object is a stupid-ass solution but why are you reading the fucking source code?
+                    emoji: "◀",
+                    style: "PRIMARY"
+                }, {
+                    type: "BUTTON",
+                    customId: `commands.leaderboard.next.${dataString}`,
+                    emoji: "▶",
+                    style: "PRIMARY"
+                }]
+            }]
+        }
+
+    if (interaction.isButton()) return reply.update(replyObj);
+    reply.edit(replyObj);
 }
