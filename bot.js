10e8651de121bc962826a922b5a065af8624ae5e
TheColorman <thoren.alex@gmail.com>
2021-09-05 21:08:23 +0200

    :arrow_up: Updated to V3.0.1, bug fix update

--- bot.js
+++ bot.js
@@ -46,7 +46,7 @@ function updateStatus() {
 
     client.user.setPresence({
         activities: [{
-            name: `V3.0.0 | ${newStatus}`,
+            name: `V3.0.1 | ${newStatus}`,
             type: 'PLAYING'
         }]
     });
