15146b124144515d89d320e161eb4b08d4740f18
TheColorman <thoren.alex@gmail.com>
2021-09-06 17:42:17 +0200

    :arrow_up: Updated to V3.0.5

--- bot.js
+++ bot.js
@@ -46,7 +46,7 @@ function updateStatus() {
 
     client.user.setPresence({
         activities: [{
-            name: `V3.0.2 | ${newStatus}`,
+            name: `V3.0.5 | ${newStatus}`,
             type: 'PLAYING'
         }]
     });
