5b505770674de4e5f0ac4a62c116fdb626839c92
Alexander Thoren <thoren.alex@gmail.com>
2021-11-06 03:29:33 +0100

    :construction: Added basic badge check

--- commands/badges.js
+++ commands/badges.js
@@ -1,5 +1,6 @@
-// eslint-disable-next-line no-unused-vars
-const Discord = require("discord.js"),
+const
+    // eslint-disable-next-line no-unused-vars
+    Discord = require("discord.js"),
     // eslint-disable-next-line no-unused-vars
     MySQL = require("../sql/sql");
 
@@ -12,11 +13,35 @@ module.exports = {
     /**
      * Command execution
      * @param {Discord.CommandInteraction} interaction Interaction object
-     * @param {MySQL} _sql MySQL custom object
+     * @param {MySQL} sql MySQL custom object
      */
-    async execute(interaction, _sql) {
-        interaction.reply({
-            content: `Badges are currently work in progress! DM ${await interaction.client.users.fetch("298842558610800650")} for a chance to get a unique badge when the system is complete!`
-        });
-    },
-};
+    async execute(interaction, sql) {
+        const
+            // Fetch DBUser
+            DBUser = await sql.getDBUser(interaction.user),
+
+            // Fetch badges
+            userBadgesField = DBUser.badges,
+            userBadgesList = [],
+
+            allBadges = await sql.get(`badges`);
+
+        // Get user badges
+        for (const badge of allBadges) {
+            if (userBadgesField & badge.id) {
+                userBadgesList.push(badge);
+            }
+        }
+
+        // Send message
+        let badgesText = userBadgesList.map(badge => `${badge.idEmoji}`).join(" ");
+        if (badgesText == "") badgesText = "You don't have any badges!";
+
+        interaction.reply({ 
+            embeds: [{
+                title: "Badges",
+                description: badgesText,
+            }],
+        })
+    }
+}
