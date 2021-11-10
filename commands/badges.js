3c82c0ed9119261d49559c69be827944d7c7da39
Alexander Thoren <thoren.alex@gmail.com>
2021-11-10 22:54:29 +0100

    :recycle: Refactored

--- commands/badges.js
+++ commands/badges.js
@@ -9,7 +9,13 @@ module.exports = {
     ApplicationCommandData: {
         name: "badges",
         description: "Displays information about badges",
+        type: "CHAT_INPUT",
         options: [{
+            type: "STRING",
+            name: "options",
+            description: "Badge command options",
+            required: false,
+        }, {
             type: "USER",
             name: "user",
             description: "User to check badges for.",
@@ -22,22 +28,46 @@ module.exports = {
      * @param {MySQL} sql MySQL custom object
      */
     async execute(interaction, sql) {
-        const
-            member = interaction.options.data[0] ? interaction.options.data[0].member : interaction.member,
+        const option = interaction.options.getString("options");
+        const member = interaction.options.getMember("user");
+        interaction.member = await member || interaction.member;
 
+        switch (option) {
+            } default: {
+                sendDefault(interaction, sql);
+                break;
+            }
+        }
+    }
+}
 
-            // Fetch DBUser
-            DBUser = await sql.getDBUser(member.user),
+/** 
+ * @param {Discord.CommandInteraction} interaction 
+ * @param {MySQL} sql
+ */
+const sendDefault = async (interaction, sql) => {
 
-            // Fetch badges
-            userBadgesField = DBUser.badges,
-            allBadges = await sql.get(`badges`),
-            userBadgesList = allBadges.filter(badge => userBadgesField & badge.bitId);
+    // Fetch DBUser
+    const DBUser = await sql.getDBUser(interaction.member.user);
 
+    // Fetch badges
+    const userBadgesField = DBUser.badges;
+    const allBadges = await sql.getDBBadges();
+    const badgesHas = allBadges.filter(badge => userBadgesField & badge.bitId);
 
-        // Send message
-        let badgesText = userBadgesList.map(badge => `${badge.idEmoji}`).join(" ");
-        if (badgesText == "") badgesText = "You don't have any badges!";
+    // Create text
+    let badgesText = badgesHas.map(badge => `${badge.idEmoji}`).join(" ");
+    badgesText ||= "You don't have any badges!";
+        
+    // Send message
+    interaction.reply({ 
+        content: badgesText,
+        embeds: [{
+            description: `${interaction.member.user}s badges.\n\`/badges options:info\` | Info\n\`/badges options:set\` | Set displayed badges\n\`/badges options:progress\` | See badge progress`
+        }],
+        color: 0x7BA043,
+    });
+}
 
         interaction.reply({ 
             embeds: [{
