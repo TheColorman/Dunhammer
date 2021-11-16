b45a173fba4c9d6119d53342acfe58bba6a0ccbb
TheColorman <thoren.alex@gmail.com>
2021-11-16 16:55:38 +0100

    :hammer: Increased request count before timeout

--- WebServer/server.js
+++ WebServer/server.js
@@ -13,7 +13,7 @@ const express = require('express'),
     { clientId: client_id } = require('../token.json'),
     limiter = rateLimit({
         windowMs: 15 * 60 * 1000, // 15 minutes
-        max: 100, // limit each IP to 100 requests per windowMs
+        max: 1000, // limit each IP to 1000 requests per windowMs
         message: "Too many requests from this IP, please try again after 15 minutes"
     }),
 
